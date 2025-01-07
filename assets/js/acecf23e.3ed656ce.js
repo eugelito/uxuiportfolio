16e7e16711878effb15af50a41d9c016d3a1ce96
Eugelito Troyo <eugelito@outlook.com>
2025-01-07 23:15:34 +0000

    Deploy website - based on 83dd7d7f379ed2cbc79d443005366a32f3778521

---
+++ assets/js/acecf23e.3ed656ce.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkuxuiportfolio=self.webpackChunkuxuiportfolio||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/uxuiportfolio/blog","blogTitle":"Blog","authorsListPath":"/uxuiportfolio/blog/authors"}')}}]);
---
+++ assets/js/cad1a147.2eea87d4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkuxuiportfolio=self.webpackChunkuxuiportfolio||[]).push([[331],{1892:o=>{o.exports=JSON.parse('{"metadata":{"permalink":"/uxuiportfolio/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
